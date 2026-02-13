1128577f6daf6e62fb89efe03c97ed0d7fb7116c
nafhansa <nafhan1723@gmail.com>
2026-02-14 01:30:25 +0700

    Revert "fix: fixed type error as any"
    This reverts commit 26f02f82d474fa451e6c1c5cc94c1aae2c039f33.

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -38,7 +38,7 @@ export class AuthenticationController {
   @Get('me')
   async getMe(@Req() req: Request) {
     return await this.authenticationService.getMyProfile(
-      req.user!.userId,
+      (req.user as any).userId as string,
     );
   }
 
